4cbf7056a653ce38a8d4fc6cc1a09c5cbcc88f46
vegapunkk <senjuruben@gmail.com>
2022-09-05 14:55:54 -0300

    update 05/09

--- core/views.py
+++ core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Autor, Categoria, Editora, Livro
+from core.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer, LivroDetailSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    # serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return LivroDetailSerializer
+        return LivroSerializer
