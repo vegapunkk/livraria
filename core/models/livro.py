a82cf10488302307f194d4f06c347e5a2bb62b2c
vegapunkk <senjuruben@gmail.com>
2022-12-17 21:39:03 -0300

    Commit Aula 20

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from uploader.models import Image
 from core.models import Autor, Categoria, Editora
+from uploader.models import Image
 
 
 class Livro(models.Model):
